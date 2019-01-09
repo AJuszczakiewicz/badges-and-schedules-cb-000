c6dc3c89c08376fc0a7ed7ac095ae8a8acce813b
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-09 18:45:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,9 @@
 # Write your code here.
 
 def badge_maker(attendee_name)
+  "Hello, my name is #{attendee_name}"
 end
 
 def batch_badge_creator(names)
+  names.map{ |name| badge_maker(name) }
 end
